5f7be34049d6566cea8a623f41b91c3d2d9fced7
sandeepnellutla <sandeep.nellutla@gmail.com>
2017-04-26 17:40:13 -0400

    done
    done

---
+++ BusinessLogic/BLFactory.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PVCodeChallenge.BusinessLogic
+{
+    class BLFactory
+    {
+        public Dictionary<int, string> GetNumbersForRegister(int minNum, int maxNum)
+        {
+
+            var multiplesof3 = Enumerable.Range(1, 100)
+                                                        .Where(x => x % 3 == 0)
+                                                        .Where(x => x % 5 != 0)
+                                                        .ToDictionary(key => key, value => "'Register'");
+            var multiplesof5 = Enumerable.Range(1, 100)
+                                                    .Where(x => x % 5 == 0)
+                                                    .Where(x => x % 3 != 0)
+                                                    .ToDictionary(key => key, value => "'Patient'");
+            var multiplesofboth = Enumerable.Range(1, 100)
+                                                            .Where(x => x % 3 == 0)
+                                                            .Where(x => x % 5 == 0)
+                                                            .ToDictionary(key => key, value => "'Register'&'Patient'");
+            var multiplesofnone = Enumerable.Range(1, 100)
+                                                            .Where(x => x % 3 != 0)
+                                                            .Where(x => x % 5 != 0)
+                                                            .ToDictionary(key => key, value => value.ToString());
+
+            Dictionary<int, string> sortedresults = ((multiplesof3.Concat(multiplesof5)).Concat(multiplesofboth)).Concat(multiplesofnone)
+                                                .OrderBy(x => x.Key)
+                                                .ToDictionary(x => x.Key, y => y.Value);
+
+            return sortedresults;
+        }
+
+        public Dictionary<int, string> GetNumbersForDiagnose(int minNum, int maxNum)
+        {
+
+            var multiplesof2 = Enumerable.Range(1, 100)
+                                                        .Where(x => x % 2 == 0)
+                                                        .Where(x => x % 7 != 0)
+                                                        .ToDictionary(key => key, value => "'Diagnose'");
+            var multiplesof7 = Enumerable.Range(1, 100)
+                                                    .Where(x => x % 7 == 0)
+                                                    .Where(x => x % 2 != 0)
+                                                    .ToDictionary(key => key, value => "'Patient'");
+            var multiplesofboth = Enumerable.Range(1, 100)
+                                                            .Where(x => x % 2 == 0)
+                                                            .Where(x => x % 7 == 0)
+                                                            .ToDictionary(key => key, value => "'Diagnose'&'Patient'");
+            var multiplesofnone = Enumerable.Range(1, 100)
+                                                            .Where(x => x % 2 != 0)
+                                                            .Where(x => x % 7 != 0)
+                                                            .ToDictionary(key => key, value => value.ToString());
+
+            Dictionary<int, string> sortedresults = ((multiplesof2.Concat(multiplesof7)).Concat(multiplesofboth)).Concat(multiplesofnone)
+                                                .OrderBy(x => x.Key)
+                                                .ToDictionary(x => x.Key, y => y.Value);
+
+            return sortedresults;
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PVCodeChallenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Dictionary<int, string> EventResults= new Dictionary<int, string>();
+
+            switch (cbOptions.Text)
+            {
+                case "'Register'": EventResults = (new BusinessLogic.BLFactory()).GetNumbersForRegister(0, 100); break;
+                case "'Diagnose'": EventResults = (new BusinessLogic.BLFactory()).GetNumbersForDiagnose(0, 100); break;
+                default: break;
+            }
+
+            dataGrid.ItemsSource = EventResults;
+        }
+    }
+}
